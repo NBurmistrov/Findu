c006d321f784449bcde236ed0a3b03e3e35044d1
W7NN3R <nick.2001.60@gmail.com>
2020-02-22 14:34:23 +0600

    Update models

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceStack.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        [PrimaryKey]
+        public int CategorysId { get; set; }
+        public virtual IEnumerable<LostItem> LostItems { get; set; }
+
+    }
+}
